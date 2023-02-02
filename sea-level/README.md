9492501f6470b87129c162be66bc1897d59f9d62
Goh Yeow Chong <gohyc1993@gmail.com>
2023-02-02 22:32:37 +0800

    fix: sea level prediction all tests passing in local terminal

---
+++ sea-level/README.md
@@ -0,0 +1,9 @@
+
+## Note
+
+To see the tests in action to verify the pass status, download all these files into one folder (Do not fork or `git` anything! That is a complete waste of time!), load an environment with `numpy`, `pandas`, `scipy` and `matplotlib`, then use `python3 main.py`.
+
+This is a temporary alternative to `replit.com` because for whatever reason it is near impossible to get `main.py` to run there.
+The environment initialisation command there takes forever to run and it is not guaranteed to complete successfully.
+I variously either get `ModuleNotFoundError` for `pandas` or `matplotlib`.
+While I can install them using `pip` in the `shell`, it causes dependency conflicts which results in `main.py` not necessarily running at all.
--- sea-level/sea_level_predictor.py
+++ sea-level/sea_level_predictor.py
@@ -4,20 +4,26 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-
-
-    # Create first line of best fit
-
-
-    # Create second line of best fit
-
-
-    # Add labels and title
-
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
     
+    # Create first line of best fit
+    ols_1 = linregress(df[['Year', 'CSIRO Adjusted Sea Level']])
+    range_1 = pd.RangeIndex(start=df['Year'].min(), stop=2051, step=1)
+    plt.plot(range_1, range_1 * ols_1.slope + ols_1.intercept, '-r')
+    
+    # Create second line of best fit
+    ols_2 = linregress(df[df['Year']>=2000][['Year', 'CSIRO Adjusted Sea Level']])
+    range_2 = pd.RangeIndex(start=2000, stop=2051, step=1)
+    plt.plot(range_2, range_2 * ols_2.slope + ols_2.intercept, '-b')
+    
+    # Add labels and title
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
